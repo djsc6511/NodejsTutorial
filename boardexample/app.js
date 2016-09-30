17414e2faa4bf9421e54e5ae6ed4eef74099bb35
djsc6511 <djsc6511@naver.com>
2016-09-30 20:01:44 +0900

    body parser 설치

--- boardexample/app.js
+++ boardexample/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 //connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
@@ -18,6 +18,7 @@ app.set("view engine",'ejs');
 
 //set middlewares
 app.use(express.static(path.join(__dirname + 'public')));
+app.user(bodyParser.json());
 
 //setart server
 app.listen(3000, function() {
